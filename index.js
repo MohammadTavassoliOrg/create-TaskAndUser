c20fb456b9248f278fee39a881ad51b15db60f83
mohammadtavasoli <mohammadtavasoliprogramming@gmail.com>
2021-10-27 04:30:20 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // file name => index
-const mmd = require("express-async-errors");
+const winston = require("winston");
+require("express-async-errors");
 const error = require("./routes/middleware/error");
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -10,6 +11,9 @@ const tasks = require("./routes/tasks");
 const express = require('express');
 const app = express();
 require("dotenv").config();
+
+winston.add(winston.transports.File, { __filename: "logfile.log" } );
+
 if (!process.env.jwtPrivateKey) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
