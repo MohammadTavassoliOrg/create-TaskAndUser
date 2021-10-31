cacf87e4242fc05229bf9dc6091bd53586945f66
mohammadtavasoli <mohammadtavasoliprogramming@gmail.com>
2021-10-30 20:24:40 -0700

    initial commit

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,8 @@
 const winston = require("winston");
 const mongoose = require('mongoose');
 
+let host = process.env.HOST;
+
 module.exports = function () {
     mongoose.connect(`mongodb://${host}/vidly`)
         .then(() => winston.info('Connected to MongoDB...'))
